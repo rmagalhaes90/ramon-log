607ceac7a4af44eaff5a8f5fdd1c73e5906004f1
rmagalhaes90 <54084809+rmagalhaes90@users.noreply.github.com>
2026-07-19 22:14:43 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "ramon-log-v1";
+const CACHE_NAME = "ramon-log-v2";
 const ASSETS = [
   "./index.html",
   "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
