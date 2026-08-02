e10ef845e2c3dd2f409b7a2f3727b82684a6f6f3
Ramon Magalhaes <rmagalhaes90@gmail.com>
2026-08-02 21:19:05 +0100

    feat: add camera barcode and offline photo queue

--- app-v4/public/sw.js
+++ app-v4/public/sw.js
@@ -1,4 +1,4 @@
-const VERSION = '4.0.0-alpha.3';
+const VERSION = '4.0.0-alpha.4';
 const SHELL = `kyro-v4-shell-${VERSION}`;
 const PRECACHE = ['./', './index.html', './manifest.webmanifest', './version.json'];
 
